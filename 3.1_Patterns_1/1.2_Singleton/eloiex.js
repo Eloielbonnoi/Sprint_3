f645caffb1dc167ec47d4fa9b56940c636de36e3
Eloi Isern <nuntxaku@gmail.com>
2022-01-19 09:49:38 +0100

    creació repo

---
+++ 3.1_Patterns_1/1.2_Singleton/eloiex.js
@@ -0,0 +1,27 @@
+/*
+PATRÓ SINGLETON
+serveix per assegurar que una classe només és instanciada una vegada creant tan sols un objecte de la classe. Aquest té una accessibilitat global. 
+Se solen usar per 
+Construeix una aplicació que creï diversos Jugadors. Els jugadors podran ser afegits a un Joc, que mostrarà un marcador amb les puntuacions i el guanyador. 
+L'aplicació ha de poder afegir o treure punts a cada jugador perquè el marcador canviï. 
+La classe Marcador ha d'implementar un patró Singleton com a requisit indispensable.
+Classe jugador
+classe marcador amb patró singleton. 
+Al main hi ha d'haver mètode crear jugador que invoqui la classe jugador i el seu constructor
+Al main mètode afegir i treure punts (potser 2 mètodes --> canvia marcador)
+*/
+var jugadors = [
+    {
+        nom : "",
+        punts : 0
+    },
+    {
+        nom : "",
+        punts : 0
+    }
+]
+
+jugadors[0]['nom'] = prompt('nom del jugador 1');
+jugadors[1]['nom'] = prompt('nom del jugador 2');
+
+console.log("aviam els noms: ", jugadors);
